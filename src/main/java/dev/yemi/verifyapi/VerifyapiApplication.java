674fdaab52dc48fd22f31e902a7672136a862a26
Yemi Oduyemi <yhermii@gmail.com>
2023-12-11 17:12:34 +0100

    Initial commit

---
+++ src/main/java/dev/yemi/verifyapi/VerifyapiApplication.java
@@ -0,0 +1,13 @@
+package dev.yemi.verifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yemi/verifyapi/VerifyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yemi.verifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
